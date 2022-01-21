253c4b954afea5d8822f5ca361770eec655887af
Simeon Griggs <simeon@hey.com>
2022-01-21 13:15:28 +0000

    add language filter to labelGroup

--- studio/src/languageFilterConfig.js
+++ studio/src/languageFilterConfig.js
@@ -5,7 +5,7 @@ const supportedLanguages = i18n.languages.map(({id, title}) => ({id, title}))
 export default {
   supportedLanguages,
   defaultLanguages: [i18n.base],
-  documentTypes: [`presenter`, `course`],
+  documentTypes: [`presenter`, `course`, `labelGroup`],
   filterField: (enclosingType, field, selectedLanguageIds) =>
     !enclosingType.name.startsWith('localized') || selectedLanguageIds.includes(field.name),
 }
